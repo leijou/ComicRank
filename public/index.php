5a82fc232079a493c61423e7d581bfb7dba6ddde
Steve H <git@leijou.com>
2013-01-12 15:12:12 +0000

    Serve favicons locally

--- public/index.php
+++ public/index.php
@@ -26,8 +26,8 @@ $page->displayHeader();
 	
 	<p>Alternatively you can follow Comic Rank:</p>
 	<ul class="sitelinks">
-		<li><a href="https://twitter.com/comicrank"><img src="https://twitter.com/favicon.ico" alt="" /> @ComicRank</a></li>
-		<li><a href="https://plus.google.com/u/0/108661948674027877061"><img src="http://plus.google.com/favicon.ico" alt="" /> +Comic Rank</a></li>
+		<li><a href="https://twitter.com/comicrank"><img src="<?=URL_STATIC?>/images/twitter.com.ico" alt="" /> @ComicRank</a></li>
+		<li><a href="https://plus.google.com/u/0/108661948674027877061"><img src="<?=URL_STATIC?>/images/plus.google.com.ico" alt="" /> +Comic Rank</a></li>
 	</ul>
 </section>
 
@@ -40,8 +40,8 @@ $page->displayHeader();
 	<p>Due to an overwhelming amount of moderation activity required to keep up with demand Comic Rank's public leaderboards were disabled in September 2012.</p>
 	<p>We're working on getting public comic listings back, and <a href="/about.php">many other things</a> too. In the meantime you might like to browse through free webcomics on these fine sites:</p>
 	<ul class="sitelinks">
-		<li><a href="http://www.comic-rocket.com/"><img src="https://www.comic-rocket.com/favicon.ico" alt="" /> Comic Rocket</a></li>
-		<li><a href="http://inkoutbreak.com/"><img src="http://inkoutbreak.com/favicon.ico" alt="" /> inkOUTBREAK</a></li>
+		<li><a href="http://www.comic-rocket.com/"><img src="<?=URL_STATIC?>/images/comic-rocket.com.ico" alt="" /> Comic Rocket</a></li>
+		<li><a href="http://inkoutbreak.com/"><img src="<?=URL_STATIC?>/images/inkoutbreak.com.ico" alt="" /> inkOUTBREAK</a></li>
 	</ul>
 </section>
 
